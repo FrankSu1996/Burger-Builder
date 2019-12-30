bb4d967d957ee61cf4f23ead6b3fdaca7f59c46c
Frank Su <franksu49@gmail.com>
2019-12-30 14:03:25 -0500

    build controls displays properly

--- burger-builder/src/components/Burger/BuildControls/BuildControls.jsx
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,3 +1,21 @@
 import React from "react";
+import styles from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
 
-const buildControls = props => <div></div>;
+//array of controls
+const controls = [
+  { label: "Lettuce", type: "lettuce" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
+const buildControls = props => (
+  <div className={styles.BuildControls}>
+    {controls.map(ctrl => {
+      return <BuildControl key={ctrl.label} label={ctrl.label} />;
+    })}
+  </div>
+);
+
+export default buildControls;
