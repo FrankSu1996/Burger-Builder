b6e32ca10cf2b94ec678491d104a62d87a731b99
Frank Su <franksu49@gmail.com>
2019-12-30 11:58:28 -0500

    removed unnecessary files

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,14 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-    );
-  }
+function App() {
+  return <div className="App"></div>;
 }
 
 export default App;
