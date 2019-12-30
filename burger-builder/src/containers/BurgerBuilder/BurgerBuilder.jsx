f9f7e6a063fd2ddea36ff2e1ef11dc27bc113773
Frank Su <franksu49@gmail.com>
2019-12-30 14:38:05 -0500

    disabled remove button if no ingredients of that type

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -38,7 +38,13 @@ class BurgerBuilder extends Component {
 
   //handles state change for removal of a single ingredient
   removeIngredientHanlder = type => {
-    const updatedCount = this.state.ingredients[type] - 1;
+    const oldCount = this.state.ingredients[type];
+
+    if (oldCount <= 0) {
+      return;
+    }
+
+    const updatedCount = oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients
     };
@@ -52,12 +58,21 @@ class BurgerBuilder extends Component {
   };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <React.Fragment>
         <Burger ingredients={this.state.ingredients}></Burger>
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHanlder}
+          disabled={disabledInfo}
         />
       </React.Fragment>
     );
