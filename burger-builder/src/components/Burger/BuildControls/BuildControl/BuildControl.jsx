fd2dee65fb4478dde4ec484d7bdcdb80671fef85
Frank Su <franksu49@gmail.com>
2019-12-30 13:57:41 -0500

    added buildControls styling and component structure

---
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./BuildControl.module.css";
+
+const buildControl = props => (
+  <div className={styles.BuildControl}>
+    <div className={styles.Label}>{props.label}</div>
+    <button className={styles.Remove}>Remove</button>
+    <button className={styles.Add}>Add</button>
+  </div>
+);
+
+export default buildControl;
--- burger-builder/src/components/Burger/Burger.jsx
+++ burger-builder/src/components/Burger/Burger.jsx
@@ -4,14 +4,20 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
   //first get keys from ingredient prop (passed as object)
-  const ingredientsTransformed = Object.keys(props.ingredients).map(
-    ingredient => {
-      //return Burger Ingredient component
+  let ingredientsTransformed = Object.keys(props.ingredients)
+    .map(ingredient => {
+      //first map to array of array of burger ingredients
       return [...Array(props.ingredients[ingredient])].map((_, index) => {
         return <BurgerIngredient key={ingredient + index} type={ingredient} />;
       });
-    }
-  );
+    })
+    //then reduce to single array of burger ingredients
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (ingredientsTransformed.length === 0) {
+    ingredientsTransformed = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top"></BurgerIngredient>
