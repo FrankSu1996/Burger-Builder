438858976eb6ddb34bcf457c63cc4c56aa61714d
Frank Su <franksu49@gmail.com>
2020-01-01 14:16:52 -0500

    added toggle functionality to sidedrawer

--- burger-builder/src/components/Layout/Layout.jsx
+++ burger-builder/src/components/Layout/Layout.jsx
@@ -5,17 +5,23 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState({ showSideDrawer: !this.state.showSideDrawer });
   };
 
   sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
   };
 
   render() {
     return (
       <React.Fragment>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
