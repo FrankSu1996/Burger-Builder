0d4bb3ae2ec9896558e5aa9e9b9266e004f3f569
Frank Su <franksu49@gmail.com>
2020-01-01 14:52:32 -0500

    restructured folders

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.jsx
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "../../UI/Modal/Modal.module.css";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+      <React.Fragment>
+        <div className={styles.ModalHeader}>
+          <h3>Your Order</h3>
+        </div>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>
+          CONTINUE
+        </Button>
+      </React.Fragment>
     );
-  });
+  }
+}
 
-  return (
-    <React.Fragment>
-      <div className={styles.ModalHeader}>
-        <h3>Your Order</h3>
-      </div>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>
-        CONTINUE
-      </Button>
-    </React.Fragment>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
--- burger-builder/src/components/UI/Modal/Modal.jsx
+++ burger-builder/src/components/UI/Modal/Modal.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = props => (
-  <React.Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
-    <div
-      className={styles.Modal}
-      style={{
-        display: props.show ? "inline" : "none",
-        transform: props.show ? "translateY(0)" : "translateY(-11vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
-  </React.Fragment>
-);
+class Modal extends Component {
+  //only render modal if state of show props changes
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop
+          show={this.props.show}
+          clicked={this.props.modalClosed}
+        ></Backdrop>
+        <div
+          className={styles.Modal}
+          style={{
+            display: this.props.show ? "inline" : "none",
+            transform: this.props.show ? "translateY(0)" : "translateY(-11vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Modal;
