705cb56598c68b7b085ccde4913be747d7960510
Frank <franksu49@gmail.com>
2020-04-26 18:10:59 -0400

    hooked up order handler with db

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
-import Burger from "../../components/Burger/Burger";
-import BuildControls from "../../components/Burger/BuildControls/BuildControls";
-import Modal from "../../components/UI/Modal/Modal";
-import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import React, {Component} from 'react';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 //mapping of ingredients to prices
 const INGREDIENT_PRICES = {
   lettuce: 0.3,
   cheese: 0.5,
   meat: 1.3,
-  bacon: 0.8
+  bacon: 0.8,
 };
 
 class BurgerBuilder extends Component {
@@ -18,13 +19,13 @@ class BurgerBuilder extends Component {
       lettuce: 0,
       bacon: 0,
       cheese: 0,
-      meat: 0
+      meat: 0,
     },
     totalPrice: 3,
     //purchasable = can burger be purchased?
     //purchasing = order now button clicked, displays modal with order summary
     purchasable: false,
-    purchasing: false
+    purchasing: false,
   };
 
   //function to update purchasable state attribute if burger has at least 1 ingredient
@@ -33,27 +34,27 @@ class BurgerBuilder extends Component {
     const sum = Object.values(ingredients).reduce((sum, el) => {
       return (sum += el);
     }, 0);
-    this.setState({ purchasable: sum > 0 });
+    this.setState({purchasable: sum > 0});
   }
 
   //handles state change for addition of a single ingredient
-  addIngredientHandler = type => {
+  addIngredientHandler = (type) => {
     const updatedCount = this.state.ingredients[type] + 1;
     const updatedIngredients = {
-      ...this.state.ingredients
+      ...this.state.ingredients,
     };
 
     updatedIngredients[type] = updatedCount;
     const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
     this.setState({
       totalPrice: updatedPrice,
-      ingredients: updatedIngredients
+      ingredients: updatedIngredients,
     });
     this.updatePurchaseState(updatedIngredients);
   };
 
   //handles state change for removal of a single ingredient
-  removeIngredientHanlder = type => {
+  removeIngredientHanlder = (type) => {
     const oldCount = this.state.ingredients[type];
 
     if (oldCount <= 0) {
@@ -62,33 +63,50 @@ class BurgerBuilder extends Component {
 
     const updatedCount = oldCount - 1;
     const updatedIngredients = {
-      ...this.state.ingredients
+      ...this.state.ingredients,
     };
 
     updatedIngredients[type] = updatedCount;
     const updatedPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
     this.setState({
       totalPrice: updatedPrice,
-      ingredients: updatedIngredients
+      ingredients: updatedIngredients,
     });
     this.updatePurchaseState(updatedIngredients);
   };
 
   purchaseHandler = () => {
-    this.setState({ purchasing: true });
+    this.setState({purchasing: true});
   };
 
   purchaseCancelHandler = () => {
-    this.setState({ purchasing: false });
+    this.setState({purchasing: false});
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Frank Su',
+        address: {
+          street: '136 Whitney',
+          postalCode: 'L8S2G7',
+          country: 'Canada',
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'Fastest',
+    };
+    axios
+      .post('/orders.json', order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
     const disabledInfo = {
-      ...this.state.ingredients
+      ...this.state.ingredients,
     };
 
     for (let key in disabledInfo) {
