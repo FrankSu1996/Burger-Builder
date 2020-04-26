54d9fa10fac9c7261ea93194c3b22147cdc7ecb6
Frank <franksu49@gmail.com>
2020-04-26 18:55:44 -0400

    added error handling modal

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -5,6 +5,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 //mapping of ingredients to prices
 const INGREDIENT_PRICES = {
@@ -157,4 +158,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
--- burger-builder/src/hoc/Layout/Layout.jsx
+++ burger-builder/src/hoc/Layout/Layout.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import styles from "./Layout.module.css";
-import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import React, {Component} from 'react';
+import styles from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: false
+    showSideDrawer: false,
   };
 
   sideDrawerToggleHandler = () => {
-    this.setState({ showSideDrawer: !this.state.showSideDrawer });
+    this.setState({showSideDrawer: !this.state.showSideDrawer});
   };
 
   sideDrawerClosedHandler = () => {
-    this.setState(prevState => {
-      return { showSideDrawer: !prevState.showSideDrawer };
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer};
     });
   };
 
